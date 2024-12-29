9fa0e0b68371b44d7f4451ae7b4cb0ef311b793e
Ali Tawoos <alitawoos350@gamil.com>
2024-12-29 21:14:44 +0200

    add exe.py file

---
+++ exe.py
@@ -0,0 +1,10 @@
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+if __name__ == "__main__":
+    print(greet("World"))
+    print(farewell("World"))
